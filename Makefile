e518b5d8c11c8d08fdc1461ecce6a8529bed824e
Admunsen55 <androneandrei20035@gmail.com>
2024-08-21 21:33:32 +0300

    Why not main

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
-all:
-	g++ -I src/include -L src/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2
+SOURCES = main.cpp utils.cpp game_interface.cpp
+OUTPUT = tic_tac_toe
+
+CXX = g++
+CXXFLAGS = -I src/include
+LDFLAGS = -L src/lib -lmingw32 -lSDL2main -lSDL2 
+
+$(OUTPUT): $(SOURCES)
+	$(CXX) $(CXXFLAGS) -o $(OUTPUT) $(SOURCES) $(LDFLAGS)
+
+clean:
+	rm $(OUTPUT)
--- main.cpp
+++ main.cpp
@@ -1,7 +1,82 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    
+#include "custom/utils.h"
+#include "custom/game_interface.h"
+
+void exit_game(SDL_Window* window, SDL_Renderer* renderer) {
+    SDL_DestroyRenderer(renderer);    
+    SDL_DestroyWindow(window);
+    SDL_Quit();    
 }
 
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("TicTacToe", 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        480, 
+        360,
+        SDL_WINDOW_SHOWN
+    );
+    if (window == nullptr) {
+        std::cerr << "Could not create window: " << SDL_GetError();
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,
+        -1, 
+        SDL_RENDERER_ACCELERATED
+    );
+    if (renderer == nullptr) {
+        std::cerr << "Could not create renderer: " << SDL_GetError();
+        return 1;
+    }
+
+    GameGrid game_table(renderer, 30, 3, 
+        {255, 255, 255, SDL_ALPHA_OPAQUE}, // grid color - white
+        {255, 0, 0, SDL_ALPHA_OPAQUE}, // X color - red
+        {0, 0, 255, SDL_ALPHA_OPAQUE}, // 0 color - blue
+        {0, 255, 0, SDL_ALPHA_OPAQUE} // win cross color - green
+    );
+
+    ////
+    game_table.set_0(1, 1);
+    game_table.set_X(1, 2);
+    ////
+    int mouseX;
+    int mouseY;
+
+    bool run_game = true;
+    SDL_Event event;
+
+    while(run_game) { 
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                run_game = false;
+            }
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
+                mouseX = event.button.x;
+                mouseY = event.button.y;
+
+                int row, column;
+                bool ok = game_table.check_mouse_cell({mouseX, mouseY}, row, column);
+                std::cout << ok << " " << row << " " << column << "\n";
+            }
+        }
+
+        // clear renderer before drawing
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); // black background
+        SDL_RenderClear(renderer);
+
+        // main logic of game
+        game_table.draw_grid();
+
+        // update renderer to display image
+        SDL_RenderPresent(renderer);
+        SDL_Delay(25);
+    }
+
+    exit_game(window, renderer);
+    return 0;
+}
